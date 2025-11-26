43422a740dc56170a69632d01695e712a6ead4bf
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-11-26 09:22:00 +0000

    Feat: add full integer formatting support (%d and %i) with new print_int and print_number handlers, updated dispatch table, and header prototypes

--- print_int.c
+++ print_int.c
@@ -4,51 +4,26 @@
  * print_int - Prints an integer
  * @args: List of variadic arguments
  *
- * Description: Extracts an integer from the argument list and prints it.
- * Handles negative numbers and prints each digit recursively.
  * Return: Number of characters printed.
  */
 int print_int(va_list args)
 {
-    int n = va_arg(args, int);
-    int count = 0;
-    unsigned int num;
+	int n = va_arg(args, int);
+	unsigned int num;
+	int count = 0;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        count++;
-        num = -n;
-    }
-    else
-    {
-        num = n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
 
-    if (num / 10)
-        count += print_number(num / 10);
+	count += print_number(num);
 
-    _putchar((num % 10) + '0');
-    count++;
-
-    return (count);
-}
-
-/**
- * print_number - Prints an unsigned integer recursively
- * @n: Number to print
- *
- * Return: Number of printed characters.
- */
-int print_number(unsigned int n)
-{
-    int count = 0;
-
-    if (n / 10)
-        count += print_number(n / 10);
-
-    _putchar((n % 10) + '0');
-    count++;
-
-    return (count);
+	return (count);
 }
